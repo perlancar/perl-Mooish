b0e6ea870e98c434e56b89bcc46563ee4d5b96ec
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-26 10:38:12 +0700

    Initial commit

---
+++ lib/Mooish.pm
@@ -0,0 +1,27 @@
+package Mooish;
+
+# VERSION
+
+1;
+# ABSTRACT: Modules related to the Mo* modules family (Mo, Moo, Moose, Mouse)
+
+=for BEGIN_BLOCK: doc
+
+=head1 DESCRIPTION
+
+The C<Mooish> and C<MooishX> namespaces are reserved for modules related to the
+Mo* modules family (Mo, Moo, Moose, Mouse, and possibly others) instead of
+certain specific implementation.
+
+
+=head1 SEE ALSO
+
+L<Mo>, C<Mo::*>
+
+L<Moo>, L<MooX>, C<MooX::*>
+
+L<Mouse>
+
+L<Moose>, L<MooseX>, C<MooseX::*>
+
+=for END_BLOCK: doc
---
+++ lib/MooishX.pm
@@ -0,0 +1,8 @@
+package MooishX;
+
+# VERSION
+
+1;
+# ABSTRACT: Modules related to the Mo* modules family (Mo, Moo, Moose, Mouse)
+
+#INCLUDE_BLOCK lib/Mooish.pm doc
